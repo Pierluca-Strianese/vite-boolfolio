87740c92556e2c99894d9f44cf0f926ed37b02d6
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-07-20 13:08:24 +0200

    add list of project by Database

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            arrProjects: [],
+        };
+    },
+    created() {
+        axios
+            .get("http://127.0.0.1:8000/api/project")
+            .then((response) => (this.arrProjects = response.data.data));
+    },
+};
+
+</script>
+
+<template>
+    <h2> questi sono i progetti</h2>
+    <ul>
+        <li v-for="project in arrProjects" :key="project.id">
+            {{ project.title }}
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+@import "bootstrap/scss/bootstrap";
+</style>
